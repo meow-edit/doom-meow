f7c9635376a3649ab4772b4de8bd20e2d736bacf
45mg <45mm.cartridge421@slmail.me>
2024-05-27 13:34:05 +0000

    fix(meow): don't put emacs state defn in autoloads
    Unless we figure out some way to autoload `meow-emacs-mode', all this
    code needs to be run on startup.

--- config.el
+++ config.el
@@ -71,6 +71,79 @@ comments. Prevent that from running if necessary."
         my/meow-want-meow-open-below-continue-comments
       t))
 
+;;; Alternate states
+
+  ;; Use a consistent key for exiting EMACS state and `meow-temp-normal'.
+  ;; We use 'C-]' as our binding to toggle this state, both in Motion and Emacs
+  ;; states. This binding was chosen based on the notion that it is rare to use
+  ;; its default binding `abort-recursive-edit'. It is rare to encounter
+  ;; recursive editing levels outside the minibuffer, and that specific case is
+  ;; handled by `doom/escape'.
+  ;; If it is really needed, `abort-recursive-edit' is also bound to `C-x X a'.
+
+  (defvar +meow-alternate-state-key "C-]"
+    "Key to switch to an alternate state in Meow.
+- Invoke `meow-temp-normal' in Motion state
+- In EMACS state, return to previous/Motion state.")
+
+  (meow-motion-overwrite-define-key
+   (cons +meow-alternate-state-key #'meow-temp-normal))
+
+;;; Emacs state
+  ;; Meow's Motion state, as configured by the suggested bindings, is mainly
+  ;; useful in modes where moving the cursor between lines is meaningful, and
+  ;; the mode doesn't bind SPC. For anything else, it tends to get in the way.
+  ;; In such cases, it's best to configure our own bindings for the mode rather
+  ;; than relying on Motion state.
+  ;; In Meow, every mode starts in a particular state, defaulting to Motion. So,
+  ;; if we want a mode to not have any Meow bindings, it needs to start in a
+  ;; state without any bindings.
+  ;; To this end, we define a custom EMACS state to switch to. This state will
+  ;; have no bindings except one to switch back to the previous state (or to
+  ;; Motion state if the buffer started in Emacs state), and a binding to invoke
+  ;; Keypad state (we use M-SPC as it's unlikely to be bound by other modes, and
+  ;; the default binding of `cycle-spacing' probably won't be relevant in the
+  ;; special modes we're using this state for).
+
+  (defvar +meow-emacs-state--previous nil
+    "Meow state before switching to EMACS state.")
+
+  (defface meow-emacs-cursor
+    `((t (:inherit unspecified
+          :background ,(face-foreground 'warning))))
+    "BEACON cursor face."
+    :group 'meow)
+
+  (defvar meow-emacs-state-keymap
+    (let ((map (make-sparse-keymap)))
+      (define-key map (kbd +meow-alternate-state-key) #'+meow-toggle-emacs-state)
+      (define-key map (kbd "M-SPC") #'meow-keypad)
+      map)
+    "Keymap for EMACS state.
+Should only contain `+meow-toggle-emacs-state'.")
+
+  (meow-define-state emacs
+    "Meow EMACS state minor mode.
+This is a custom state having no bindings except `+meow-toggle-emacs-state' and
+`meow-keypad'."
+    :lighter " [E]"
+    :keymap meow-emacs-state-keymap
+    :face meow-emacs-cursor)
+
+  (defun +meow-toggle-emacs-state ()
+    "Toggle EMACS state.
+If EMACS state was manually switched to via this command, switch back to the
+previous state. Otherwise, assume that the buffer started in EMACS state, and
+switch to MOTION state."
+    (interactive)
+    (if (meow-emacs-mode-p)
+        (progn
+          (meow--switch-state
+           (or +meow-emacs-state--previous 'motion))
+          (setq +meow-emacs-state--previous nil))
+      (setq +meow-emacs-state--previous meow--current-state)
+      (meow--switch-state 'emacs)))
+
 ;;; misc. settings
 
   ;; Wait for longer before removing the expansion hints. One second is too
@@ -144,29 +217,10 @@ comments. Prevent that from running if necessary."
   ;; Like in Doom's evil config.
   (add-to-list 'meow-keypad-start-keys '(?u . ?u))
 
-
-;;;;; Alternate states
-
-  ;; Use a consistent key for exiting EMACS state (see autoload/emacs-state.el)
-  ;; and `meow-temp-normal'.
-  ;; We use 'C-]' as our binding to toggle this state, both in Motion and Emacs
-  ;; states. This binding was chosen based on the notion that it is rare to use
-  ;; its default binding `abort-recursive-edit'. It is rare to encounter
-  ;; recursive editing levels outside the minibuffer, and that specific case is
-  ;; handled by `doom/escape'.
-  ;; If it is really needed, `abort-recursive-edit' is also bound to `C-x X a'.
-  (defvar +meow-alternate-state-key "C-]"
-    "Key to switch to an alternate state in Meow.
-- Invoke `meow-temp-normal' in Motion state
-- In EMACS state, return to previous/Motion state.")
-
-  (meow-motion-overwrite-define-key
-   (cons +meow-alternate-state-key #'meow-temp-normal))
-
 ;;;;; 'M-SPC'
 
-  ;; Copy the behavior in :editor evil, where 'M-SPC' triggers the leader-key
-  ;; bindings in Insert state.
+  ;; As in our EMACS state, make 'M-SPC' trigger the leader-key bindings in
+  ;; Insert state.
   (meow-define-keys 'insert '("M-SPC" . meow-keypad))
 
 ;;;;; `+meow-escape'
